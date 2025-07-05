3408a42b9b4c20e04f54fde6b4bed1cb02de099d
Isaias <isaiascabrera20@gmail.com>
2025-07-05 20:36:25 -0300

    Learning about REACT + Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 
-import './globals.css';
+import './ui/globals.css';
+import { InterFont } from './ui/fonts';
 
 export default function Layout({ children }: { children: React.ReactNode }){
   return (
     <html lang="en">
-      <body>
+      <body className={`${InterFont.className} antiliased`}>
         <span>Soy un Layout</span>
         {children}
       </body>
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Raleway } from 'next/font/google'
+
+export const InterFont = Raleway({subsets: ['latin'], weight: ['400','500','600']})
