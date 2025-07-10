974ed4ccf240d80d4316f6825c970af07d87d92d
Isaias <isaiascabrera20@gmail.com>
2025-07-10 16:31:31 -0300

    Fix version 15 Next.js - Problem at SearchParams

--- src/app/anime/seasonal/page.tsx
+++ src/app/anime/seasonal/page.tsx
@@ -3,20 +3,20 @@ import { Suspense } from 'react';
 import Search from '@/components/ui/Search'
 import SeasonalAnimes from '@/components/SeasonalAnime';
 
-export default async function SeasonalPage({ searchParams }: {searchParams: {season?: string}}) {
+export default async function SeasonalPage({ searchParams }: {
+  searchParams: Promise<{season?: string}>
+}) {
 
-  const season = searchParams?.season || 'spring'
-  console.log(season)
+  const { season = 'spring' } = await searchParams
 
   return (
     <main>
       <section className='p-5 flex flex-col gap-5 bg-black'>
         <h1 className='text-5xl text-center text-white'>Temporada Animes</h1>
 
-           <Search placeholder='Season'/>
+        <Search placeholder='Season'/>
        
-
-        <Suspense key={season} fallback='Loading seasonal animes'>
+        <Suspense fallback='Loading seasonal animes'>
             <SeasonalAnimes season={season}/>
         </Suspense>
 
