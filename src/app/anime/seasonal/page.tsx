9a9be5ff439434a316db8af2c03da0f771601689
Isaias <isaiascabrera20@gmail.com>
2025-07-09 22:08:12 -0300

    Fix Seasonal View

--- src/app/anime/seasonal/page.tsx
+++ src/app/anime/seasonal/page.tsx
@@ -10,7 +10,9 @@ export default function SeasonalPage({ searchParams }: { searchParams?: { season
       <section className='p-5 flex flex-col gap-5 bg-gray-200'>
         <h1 className='text-5xl text-center text-black'>Seasonal Animes</h1>
 
-        <Search  placeholder='Season'/>
+        <Suspense>
+            <Search  placeholder='Season'/>
+        </Suspense>
 
         <Suspense fallback='Loading seasonal animes'>
             { <SeasonalAnimes searchParams={searchParams}/> }
